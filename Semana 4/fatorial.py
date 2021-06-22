dd1f05f8f235439f5a17d98fb1f1f029b29e19e5
Rodrigo Santiago <rodrigons@gmail.com>
2021-06-22 20:27:07 -0300

    otimização na função fatorial

--- Semana 4/fatorial.py
+++ Semana 4/fatorial.py
@@ -1,13 +1,9 @@
-from OneDrive.Codes.testes import fatorial
 n = int(input("Digite o valor de n: "))
 
-nfatorial = n
+nfat = 1
 
-if n == 0 or n == 1:
-    nfatorial = 1
-else:
-    while n != 1:
-        nfatorial = nfatorial * (n - 1)
-        n -= 1
+while n > 1:
+    nfat = nfat * n
+    n -= 1
 
-print (nfatorial)
+print (nfat)
