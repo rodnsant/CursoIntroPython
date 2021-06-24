ed978f6c047c42827c1bd4eb991ca30970ce8b35
Rodrigo Santiago <rodrigons@gmail.com>
2021-06-24 14:24:16 -0300

    refatoração

--- Semana 5/bhaskaradef.py
+++ Semana 5/bhaskaradef.py
@@ -1,29 +1,27 @@
 import math 
 
-def raizdelta (): 
-    return math.sqrt(delta)
+def delta(a,b,c):
+    return (b**2) - (4*a*c)
 
-def x1():
-    return (-b + raizdelta())/(2*a)
+def main():
+    a = float(input("Digite o valor de a: " ))
+    b = float(input("Digite o valor de b: " ))
+    c = float(input("Digite o valor de c: " ))
+    imprime_raizes(a,b,c)
 
-def x2():
-    return (-b - raizdelta())/(2*a)
-
-a = float(input("Digite o valor de a: " ))
-
-b = float(input("Digite o valor de b: " ))
-
-c = float(input("Digite o valor de c: " ))
-
-delta = (b**2) - (4*a*c)
-
-if delta == 0:
-    print("a raiz desta equação é",x1())
-else:
-    if delta < 0:
-        print("esta equação não possui raízes reais")
+def imprime_raizes(a,b,c):
+    d = delta(a,b,c)
+    if d == 0:
+        print("a raiz desta equação é",(-b + math.sqrt(d))/(2*a))
     else:
-        if x1() < x2():
-            print("as raízes da equação são",x1(),"e",x2())
+        if d < 0:
+            print("esta equação não possui raízes reais")
         else:
-            print("as raízes da equação são",x2(),"e",x1())
+            x1 = (-b + math.sqrt(d))/(2*a)
+            x2 = (-b - math.sqrt(d))/(2*a)
+            if x1 < x2:
+                print("as raízes da equação são",x1,"e",x2)
+            else:
+                print("as raízes da equação são",x2,"e",x1)
+
+main()
